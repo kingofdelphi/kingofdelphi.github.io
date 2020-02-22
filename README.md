f5065169e6aa989b998ca0f7f465afc1da651648
Uttam Khanal <kingofdelphi1992@gmail.com>
2020-02-22 11:19:55 +0545

    Add google analytics

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # kingofdelphi.github.io
+
+# pull sub repos
+git submodule update --init --recursive
+
+# make sure all repo have master branch as default
+git submodule foreach git checkout master
+
+# build portfolio page
+
+# use this particular node version
+# some projects use webpack 4, while some use webpack 2
+# in case any error occurs, need to build the projects individually with correct node version
+nvm use 10.0.0
+
+# to build profile page only
+./build.sh
+
+# to build sub projects as well as profile page
+./build.sh all
+
+
--- build.sh
+++ build.sh
@@ -1,30 +1,33 @@
-cd 2d_physics_engine
-yarn
-yarn build
-cd ..
+if [ "$1" = "all" ]; then
+  echo "Building dependencies"
+  cd 2d_physics_engine
+  yarn
+  yarn build
+  cd ..
 
-cd bicycle
-yarn
-yarn build
-cd ..
+  cd bicycle
+  yarn
+  yarn build
+  cd ..
 
-cd cube_solver
-yarn
-yarn build
-cd ..
+  cd cube_solver
+  yarn
+  yarn build
+  cd ..
 
-rm -rf projects
-mkdir projects
+  rm -rf projects
+  mkdir projects
 
-cp -r 2d_physics_engine/dist projects/2d_physics_engine
-cp -r bicycle/dist projects/bicycle
-cp -r cube_solver/dist projects/cube_solver
+  cp -r 2d_physics_engine/dist projects/2d_physics_engine
+  cp -r bicycle/dist projects/bicycle
+  cp -r cube_solver/dist projects/cube_solver
 
-cp -r neverball projects/neverball
-rm -r projects/neverball/.git
+  cp -r neverball projects/neverball
+  rm -r projects/neverball/.git
 
-cp -r car_game projects/car_game
-rm -r projects/car_game/.git
+  cp -r car_game projects/car_game
+  rm -r projects/car_game/.git
+fi
 
 yarn build
 
