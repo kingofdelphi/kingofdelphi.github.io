a90fe199ec5bbac36c2eaebee4a07f207f5f37f9
Uttam Khanal <kingofdelphi1992@gmail.com>
2020-03-22 13:16:45 +0545

    Update UI

--- src/Main.js
+++ src/Main.js
@@ -23,32 +23,32 @@ const Main = () => {
           <GameLink
             href="/projects/machine_learning_with_javascript"
             title="Machine Learning"
-            description="Regression and classification showcase."
+            description="Regression and classification showcase. Supports linear and non-linear regression and classification"
           />
           <GameLink
             href="/projects/cube_solver"
             title="Cube Solver"
-            description="AI solver for game from Simon Tathams Puzzle Collection"
+            description="AI solver for game from Simon Tathams Puzzle Collection. Tweaked version of Dijkstra was used to solve the problem."
           />
           <GameLink
             href="/projects/neverball"
             title="Neverball"
-            description="Open source game clone in vanilla JS"
+            description="3d game written in vanilla JS. A 3d renderer was built. It also includes a level designer as well."
           />
           <GameLink
             href="/projects/2d_physics_engine"
             title="2d Physics Engine"
-            description="Demonstrates collision detection and response"
+            description="Demonstrates collision detection and response. SAT algorithm was used for collision detection."
           />
           <GameLink
             href="/projects/bicycle"
             title="Cycle Game"
-            description="Cycle game using verlet physics"
+            description="Cycle game using verlet physics. Simple line and circle collision detection and resolution was used with verlet integration"
           />
           <GameLink
             href="/projects/car_game"
             title="2d Street Racing"
-            description="Street racing car game"
+            description="A very simple car game written in javascript."
           />
         </div>
       </div>
